6bbfb876d59b8c4aa85ac5b2c616f0c0f9394693
xd00099 <dxiang@ucsd.edu>
2022-01-04 16:23:39 -0800

    lab1 added

---
+++ labs/01-python_basics/tests/q4_3_1.py
@@ -0,0 +1,1 @@
+test = {'name': 'q4_3_1', 'points': None, 'suites': [{'cases': [{'code': '>>> new_year == 2022\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
