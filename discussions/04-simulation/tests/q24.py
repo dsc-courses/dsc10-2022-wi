a32d7b4a87d8efaf952c88960bec230d783226c7
Couson <couson@foxmail.com>
2022-01-31 03:37:05 -0800

    add disc04 to public

---
+++ discussions/04-simulation/tests/q24.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q24',
+    'points': None,
+    'suites': [   {   'cases': [   {   'code': '>>> number_of_suit([\'6 of diamonds\', \'K of clubs\', \'10 of clubs\', \'7 of spades\', \'7 of clubs\'], "clubs") == 3\nTrue',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
